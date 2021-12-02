492b5bd4c83952149b6d9779359ee62125bd1a9d
Adam Purcell <apurcell88@gmail.com>
2021-12-02 15:43:04 -0600

    Moved code from index.js into page-load.js. Did more styling.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,11 @@
 // import pageLoader from './page-load.js';
 import './style.css';
+import loadPage from './page-load';
 
 const content = document.querySelector('#content');
 
-function createHeader() {
-    const header = document.createElement('div');
-    header.classList.add('header');
-    // content.appendChild(header);
+function init() {
+    loadPage();
+}
 
-    const restaurantName = document.createElement('h1');
-    restaurantName.innerText = 'Pablo\'s Taco Bar';
-    restaurantName.classList.add('restaurant-name');
-    header.appendChild(restaurantName);
-
-    header.appendChild(createNav());
-
-    return header;
-
-};
-
-function createNav() {
-    const nav = document.createElement('nav');
-    nav.classList.add('nav');
-
-    const homeBtn = document.createElement('button');
-    homeBtn.innerText = 'Home';
-    homeBtn.classList.add('nav-btn');
-    nav.appendChild(homeBtn);
-
-    const menuBtn = document.createElement('button');
-    menuBtn.innerText = 'Menu';
-    menuBtn.classList.add('nav-btn');
-    nav.appendChild(menuBtn);
-
-    const contactBtn = document.createElement('button');
-    contactBtn.innerText = 'Contact Us';
-    contactBtn.classList.add('nav-btn');
-    nav.appendChild(contactBtn);
-
-    return nav;
-};
-
-content.appendChild(createHeader());
-// function createMain() {
-
-// }
+init();
--- src/page-load.js
+++ src/page-load.js
@@ -1,17 +1,71 @@
-import Icon from './bar.jpg';
+function createHeader() {
+    const header = document.createElement('div');
+    header.classList.add('header');
 
-export default function pageLoader(element, text) {
-    let container = document.querySelector('#content');
-    let newElement = document.createElement(element);
+    const restaurantName = document.createElement('h1');
+    restaurantName.innerText = 'Pablo\'s Taco Bar';
+    restaurantName.classList.add('restaurant-name');
+    header.appendChild(restaurantName);
 
-    console.log(newElement);
+    header.appendChild(createNav());
 
-    if (newElement.tagName === 'IMG') {
-        newElement = new Image();
-        newElement.src = Icon;
-    } else {
-        newElement.innerText = text;
-    }
+    return header;
 
-    container.appendChild(newElement);
+};
+
+function createNav() {
+    const nav = document.createElement('nav');
+    nav.classList.add('nav');
+
+    const homeBtn = document.createElement('button');
+    homeBtn.innerText = 'Home';
+    homeBtn.classList.add('nav-btn');
+    nav.appendChild(homeBtn);
+
+    const menuBtn = document.createElement('button');
+    menuBtn.innerText = 'Menu';
+    menuBtn.classList.add('nav-btn');
+    nav.appendChild(menuBtn);
+
+    const contactBtn = document.createElement('button');
+    contactBtn.innerText = 'Contact';
+    contactBtn.classList.add('nav-btn');
+    nav.appendChild(contactBtn);
+
+    return nav;
+};
+
+function createMain() {
+    const main = document.createElement('main');
+    main.classList.add('main');
+
+    const descriptionContainer = document.createElement('div');
+    descriptionContainer.classList.add('description-container');
+    main.appendChild(descriptionContainer);
+
+    const descriptionTop = document.createElement('h2');
+    descriptionTop.innerText = 'Come on in for some authentic tacos!';
+    descriptionContainer.appendChild(descriptionTop);
+
+    const descriptionBottom = document.createElement('p');
+    descriptionBottom.innerText = 'Mexico City inspired taste!';
+    descriptionContainer.appendChild(descriptionBottom);
+
+    const orderBtn = document.createElement('button');
+    orderBtn.innerText = 'Order Now';
+    descriptionContainer.appendChild(orderBtn);
+
+    return main;
+};
+
+function loadPage() {
+    const content = document.querySelector('#content');
+    
+    const header = createHeader();
+    content.appendChild(header);
+
+    const main = createMain();
+    content.appendChild(main);
 }
+
+export default loadPage;
