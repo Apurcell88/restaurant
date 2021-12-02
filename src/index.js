bd0c30f1219b9fc38f406184e6d839b4b6a3424d
Adam Purcell <apurcell88@gmail.com>
2021-12-02 14:15:32 -0600

    Header styling is complete. Added another function into index.js that creates the nav.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
 // import pageLoader from './page-load.js';
 import './style.css';
 
-(function createHeader() {
-    const content = document.querySelector('#content');
+const content = document.querySelector('#content');
 
+function createHeader() {
     const header = document.createElement('div');
     header.classList.add('header');
-    content.appendChild(header);
+    // content.appendChild(header);
 
     const restaurantName = document.createElement('h1');
     restaurantName.innerText = 'Pablo\'s Taco Bar';
     restaurantName.classList.add('restaurant-name');
     header.appendChild(restaurantName);
 
-})();
+    header.appendChild(createNav());
 
+    return header;
+
+};
+
+function createNav() {
+    const nav = document.createElement('nav');
+    nav.classList.add('nav');
+
+    const homeBtn = document.createElement('button');
+    homeBtn.innerText = 'Home';
+    homeBtn.classList.add('nav-btn');
+    nav.appendChild(homeBtn);
+
+    const menuBtn = document.createElement('button');
+    menuBtn.innerText = 'Menu';
+    menuBtn.classList.add('nav-btn');
+    nav.appendChild(menuBtn);
+
+    const contactBtn = document.createElement('button');
+    contactBtn.innerText = 'Contact Us';
+    contactBtn.classList.add('nav-btn');
+    nav.appendChild(contactBtn);
+
+    return nav;
+};
+
+content.appendChild(createHeader());
 // function createMain() {
 
 // }
